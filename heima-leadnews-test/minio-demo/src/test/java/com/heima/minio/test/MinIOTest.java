a021091a85d0cdf3b2e66b35dba8926515341eb6
fang <1461434157@qq.com>
2024-04-18 15:44:36 +0800

    使用minio存储

---
+++ heima-leadnews-test/minio-demo/src/test/java/com/heima/minio/test/MinIOTest.java
@@ -0,0 +1,61 @@
+package com.heima.minio.test;
+
+import com.heima.file.service.FileStorageService;
+import com.heima.minio.MinIOApplication;
+import io.minio.MinioClient;
+import io.minio.PutObjectArgs;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+@SpringBootTest(classes= MinIOApplication.class)
+@RunWith(SpringRunner.class)
+public class MinIOTest {
+
+
+    public static void main(String[] args) {
+
+        FileInputStream fileInputStream = null;
+        try {
+
+            fileInputStream =  new FileInputStream("D:\\plugins\\js\\axios.min.js");;
+
+            //1.创建minio链接客户端
+            MinioClient minioClient = MinioClient.builder().credentials("minio", "minio123").endpoint("http://192.168.200.130:9000").build();
+            //2.上传
+            PutObjectArgs putObjectArgs = PutObjectArgs.builder()
+                    .object("plugins/js/axios.min.js")//文件名
+                    .contentType("text/js")//文件类型
+                    .bucket("leadnews")//桶名词  与minio创建的名词一致
+                    .stream(fileInputStream, fileInputStream.available(), -1) //文件流
+                    .build();
+            minioClient.putObject(putObjectArgs);
+
+           // System.out.println("http://192.168.200.130:9000/leadnews/list.html");
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
+
+    @Autowired
+    private FileStorageService fileStorageService;
+
+    @Test
+    public void testUpdateImgFile() {
+        try {
+           FileInputStream fileInputStream=new FileInputStream("D:\\list.html");
+           String path =fileStorageService.uploadHtmlFile("","list.html",fileInputStream);
+           System.out.println(path);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
